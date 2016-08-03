c1f6749bdf13295275e4683fb97b11ff706259d4
Don Pollitz <chaos2007@gmail.com>
2016-08-02 22:27:00 -0500

    Doesn't output anything, just a working binary that doesn't do anything.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        
+        let mut message = String::new();
+        io::stdin().read_line(&mut message)
+            .expect("failed to read line");
+
+        //player <north/south> name (player <north/south> <name>)
+        //go play-card (play 1 red,3)
+        println!("{}", message);
+
+    }
 }
+
