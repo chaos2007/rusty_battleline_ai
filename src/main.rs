00c97e16bcad0f07c4fc046e469e59eeebe26ffa
Don Pollitz <chaos2007@gmail.com>
2016-08-04 13:43:57 -0500

    Enough to have running bot.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate rusty_battleline_interface as rbi;
 use std::io;
 
 
@@ -8,10 +9,20 @@ fn main() {
         io::stdin().read_line(&mut message)
             .expect("failed to read line");
 
-        //player <north/south> name (player <north/south> <name>)
-        //go play-card (play 1 red,3)
-        println!("{}", message);
-
+        let x = rbi::parse_message(message);
+        match x {
+            rbi::Message::PlayerDirection{ direction: rbi::Direction::North } => {
+                println!("player north rusty_battleline_bot");
+            },
+            rbi::Message::PlayerDirection{ direction: rbi::Direction::South } => {
+                println!("player south rusty_battleline_bot");
+            },
+            rbi::Message::PlayCard => {
+                println!("play 1 red,1");
+            },
+            _ => {
+            }
+        }
     }
 }
 
