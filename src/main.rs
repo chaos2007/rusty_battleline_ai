75397bd0f43beafbe80780cdad7894c7f31bf305
Don Pollitz <chaos2007@gmail.com>
2016-08-08 20:24:14 -0500

    Import package.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,24 @@
 extern crate rusty_battleline_interface as rbi;
 use std::io;
 
+struct Ai {
+}
+
+impl rbi::game_state::AiInterface for Ai {
+    fn update_game_state(&self, state: &rbi::game_state::GameState) -> String {
+        return String::from("play 1 red,1");
+    }
+    
+    fn get_bot_name(&self) -> String {
+        return String::from("rusty_battleline_bot");
+    }
+}
 
 fn main() {
+    let handler:rbi::game_state::GameHandler = Default::default();
+    let ai = Ai{}; 
     loop {
-        
-        let mut message = String::new();
-        io::stdin().read_line(&mut message)
-            .expect("failed to read line");
-
-        let x = rbi::parse_message(message);
-        match x {
-            rbi::Message::PlayerDirection{ direction: rbi::Direction::North } => {
-                println!("player north rusty_battleline_bot");
-            },
-            rbi::Message::PlayerDirection{ direction: rbi::Direction::South } => {
-                println!("player south rusty_battleline_bot");
-            },
-            rbi::Message::PlayCard => {
-                println!("play 1 red,1");
-            },
-            _ => {
-            }
-        }
+        handler.run_one_round(&ai);
     }
 }
 
