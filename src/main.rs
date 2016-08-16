868b9df14262b5ad0ca83549b2669824f0eacb96
Don Pollitz <chaos2007@gmail.com>
2016-08-15 22:48:45 -0500

    Trying to check for battalion.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::io;
 struct Ai {
 }
 
+
 impl rbi::game_state::AiInterface for Ai {
     fn update_game_state(&self, state: &rbi::game_state::GameState) -> String {
         let mut my_cards = state.player_hand.clone();
@@ -13,7 +14,7 @@ impl rbi::game_state::AiInterface for Ai {
         my_flags.reverse();
         my_cards.sort_by_key(|k| k.number);
         for (x, claimed) in claims.iter().enumerate().rev() {
-            if (my_flags[x].len() <= 3) {
+            if my_flags[x].len() <= 3 {
                 match *claimed {
                     rbi::message_parsing::ClaimStatus::Unclaimed => {
                         match my_cards.last() {
@@ -31,7 +32,7 @@ impl rbi::game_state::AiInterface for Ai {
     }
 
     fn get_bot_name(&self) -> String {
-        return String::from("rusty_battleline_bot");
+        return String::from("rusty_battleline_bot_wip");
     }
 }
 
@@ -47,6 +48,46 @@ fn main() {
     }
 }
 
+fn check_for_battalion(hand: Vec<rbi::message_parsing::Card>, 
+                       flag_cards: Vec<rbi::message_parsing::Card>,
+                       colors: Vec<String>) -> (bool, rbi::message_parsing::Card) {
+    let mut card:Option<rbi::message_parsing::Card> = None;
+    let hand = hand.clone();
+    let flag_cards = flag_cards.clone();
+    let colors = colors.clone();
+    for color in &colors {
+        let mut num = 0;
+        let mut tempCard:Option<rbi::message_parsing::Card> = None;
+        for x in &hand {
+            match x {
+                &rbi::message_parsing::Card{color:colora,number:numbera}=> {
+                    num += 1;
+                    match &tempCard {
+                        &Some(rbi::message_parsing::Card{color:colorb, number:numberb}) => {
+                            tempCard = Some(rbi::message_parsing::Card {color:colorb, number:numberb}) 
+                        },
+                        _ => {
+                        }
+                    }
+                }
+            }
+        }
+        for x in flag_cards.clone(){
+            match x {
+                rbi::message_parsing::Card{color:color,..} => {
+                    num += 1;
+                }
+            }
+        }
+        if num >= 3 && tempCard != None {
+            card = tempCard;
+        }
+    }
+    match card {
+        Some(ref x) => return (true,x.clone()),
+        _ => return (false, rbi::message_parsing::Card{color:String::from("a"), number:1}),
+    }
+}
 
 #[cfg(test)]
 mod test_game_state {
@@ -55,6 +96,30 @@ mod test_game_state {
     use rbi::message_parsing;
     use rbi::message_parsing as mp;
     extern crate rusty_battleline_interface as rbi;
+
+    fn get_colors() -> Vec<String>{
+        vec![String::from("a"), String::from("b"), String::from("c"),
+                                   String::from("d"), String::from("e"), String::from("f")]
+    }
+
+    #[test]
+    fn verify_battalion_1() {
+        let hand = vec![mp::Card{color:String::from("a"), number:7},
+                        mp::Card{color:String::from("a"), number:1}];
+        let flag = vec![mp::Card{color:String::from("a"), number:6}];
+        let (x, y) = super::check_for_battalion(hand, flag, get_colors());
+        assert!(x);
+        assert_eq!(mp::Card{color:String::from("a"), number:7}, y);
+    }
+    
+    #[test]
+    fn verify_battalion_2() {
+        let hand = vec![mp::Card{color:String::from("a"), number:7},
+                        mp::Card{color:String::from("b"), number:1}];
+        let flag = vec![mp::Card{color:String::from("a"), number:6}];
+        let (x, _) = super::check_for_battalion(hand, flag, get_colors());
+        assert!(!x);
+    }
 
     #[test]
     fn playHighestOnLast() {
