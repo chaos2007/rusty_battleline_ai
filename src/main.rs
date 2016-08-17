981e9a33c78e72a89221b9f906d446ed04007618
Don Pollitz <chaos2007@gmail.com>
2016-08-16 20:59:44 -0500

    Convert to using enums.

--- src/main.rs
+++ src/main.rs
@@ -11,26 +11,35 @@ impl rbi::game_state::AiInterface for Ai {
         let mut claims = state.claim_status.clone();
         let mut my_flags = state.player_side.clone();
         let mut colors = state.colors.clone();
-        //claims.reverse();
+        // claims.reverse();
         my_flags.reverse();
         my_cards.sort_by_key(|k| k.number);
+        for (x, claimed) in claims.iter().enumerate() {
+            if my_flags[x].len() <= 3 {
+                match *claimed {
+                    rbi::message_parsing::ClaimStatus::Unclaimed => {
+                        match check_for_battalion(&my_cards, &my_flags[x], &state.colors_vec) {
+                            (true, card) => {
+                                let color_string = state.string_from_color(card.color);
+                                return format!("play {} {},{}", x + 1, color_string, card.number);
+                            }
+                            _ => {}
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
         for (x, claimed) in claims.iter().enumerate().rev() {
             if my_flags[x].len() <= 3 {
                 match *claimed {
                     rbi::message_parsing::ClaimStatus::Unclaimed => {
-                        match check_for_battalion(&my_cards, &my_flags[x], &colors) {
-                            (true, card) => {
-                                return format!("play {} {},{}", x+1, card.color, card.number);
-                            },
-                            _ =>
-                            {
                         match my_cards.last() {
                             Some(card) => {
-                                return format!("play {} {},{}", x+1, card.color, card.number);
+                                let color_string = state.string_from_color(card.color);
+                                return format!("play {} {},{}", x + 1, color_string, card.number);
                             }
                             _ => {}
-                        }
-                            }
                         }
                     }
                     _ => {}
@@ -57,37 +66,39 @@ fn main() {
     }
 }
 
-fn check_for_battalion(hand: &Vec<rbi::message_parsing::Card>, 
+fn check_for_battalion(hand: &Vec<rbi::message_parsing::Card>,
                        flag_cards: &Vec<rbi::message_parsing::Card>,
-                       colors: &Vec<String>) -> (bool, rbi::message_parsing::Card) {
-    let mut card:Option<rbi::message_parsing::Card> = None;
+                       colors_vec: &Vec<rbi::message_parsing::Color>)
+                       -> (bool, rbi::message_parsing::Card) {
+    let mut card: Option<rbi::message_parsing::Card> = None;
     let hand = hand.clone();
     let flag_cards = flag_cards.clone();
-    let colors = colors.clone();
-    for color_string in &colors {
+    for color_string in colors_vec {
         let mut num = 0;
-        let mut tempCard:Option<rbi::message_parsing::Card> = None;
+        let mut tempCard: Option<rbi::message_parsing::Card> = None;
         for x in &hand {
             match x {
-                &rbi::message_parsing::Card{ref color,number} if color_string == color => {
+                &rbi::message_parsing::Card { ref color, number } if color_string == color => {
                     num += 1;
                     tempCard = match tempCard {
-                        Some(rbi::message_parsing::Card{color:ref colora, number:numbera}) => {
-                            Some(rbi::message_parsing::Card {color:(*colora).clone(), number:numbera}) 
-                        },
-                        _ => {
-                            Some(x.clone()) 
+                        Some(rbi::message_parsing::Card { color: ref colora, number: numbera }) => {
+                            Some(rbi::message_parsing::Card {
+                                color: (*colora).clone(),
+                                number: numbera,
+                            })
                         }
+                        _ => Some(x.clone()),
                     }
-                },
+                }
                 _ => {}
             }
         }
-        for x in flag_cards.clone(){
+        for x in flag_cards.clone() {
             match x {
-                rbi::message_parsing::Card{color:color_string,..} => {
+                rbi::message_parsing::Card { ref color, .. } if color_string == color => {
                     num += 1;
                 }
+                _ => {}
             }
         }
         if num >= 3 && tempCard != None {
@@ -95,8 +106,14 @@ fn check_for_battalion(hand: &Vec<rbi::message_parsing::Card>,
         }
     }
     match card {
-        Some(ref x) => return (true,x.clone()),
-        _ => return (false, rbi::message_parsing::Card{color:String::from("a"), number:1}),
+        Some(ref x) => return (true, x.clone()),
+        _ => {
+            return (false,
+                    rbi::message_parsing::Card {
+                color: rbi::message_parsing::Color::Color1,
+                number: 1,
+            })
+        }
     }
 }
 
@@ -108,46 +125,90 @@ mod test_game_state {
     use rbi::message_parsing as mp;
     extern crate rusty_battleline_interface as rbi;
 
-    fn get_colors() -> Vec<String>{
-        vec![String::from("a"), String::from("b"), String::from("c"),
-                                   String::from("d"), String::from("e"), String::from("f")]
+    fn get_colors() -> Vec<mp::Color> {
+        vec![mp::Color::Color1,
+             mp::Color::Color2,
+             mp::Color::Color3,
+             mp::Color::Color4,
+             mp::Color::Color5,
+             mp::Color::Color6]
     }
 
     #[test]
     fn verify_battalion_1() {
-        let hand = vec![mp::Card{color:String::from("a"), number:7},
-                        mp::Card{color:String::from("a"), number:1}];
-        let flag = vec![mp::Card{color:String::from("a"), number:6}];
+        let hand = vec![mp::Card {
+                            color: mp::Color::Color1,
+                            number: 7,
+                        },
+                        mp::Card {
+                            color: mp::Color::Color1,
+                            number: 1,
+                        }];
+        let flag = vec![mp::Card {
+                            color: mp::Color::Color1,
+                            number: 6,
+                        }];
         let (x, y) = super::check_for_battalion(&hand, &flag, &get_colors());
         assert!(x);
-        assert_eq!(mp::Card{color:String::from("a"), number:7}, y);
+        assert_eq!(mp::Card {
+                       color: mp::Color::Color1,
+                       number: 7,
+                   },
+                   y);
     }
-    
+
     #[test]
     fn verify_battalion_2() {
-        let hand = vec![mp::Card{color:String::from("a"), number:7},
-                        mp::Card{color:String::from("b"), number:1}];
-        let flag = vec![mp::Card{color:String::from("a"), number:6}];
+        let hand = vec![mp::Card {
+                            color: mp::Color::Color1,
+                            number: 7,
+                        },
+                        mp::Card {
+                            color: mp::Color::Color2,
+                            number: 1,
+                        }];
+        let flag = vec![mp::Card {
+                            color: mp::Color::Color1,
+                            number: 6,
+                        }];
         let (x, _) = super::check_for_battalion(&hand, &flag, &get_colors());
         assert!(!x);
     }
 
     #[test]
     fn playHighestOnLast() {
-        let ai = super::Ai{};
-        let mut state:rbi::game_state::GameHandler = Default::default();
+        let ai = super::Ai {};
+        let mut state: rbi::game_state::GameHandler = Default::default();
         state.run_one_round(&ai, String::from("colors a b c d e f"));
         state.run_one_round(&ai, String::from("player north name"));
-        state.state.player_hand = vec![mp::Card{color:String::from("a"), number:7},
-                                       mp::Card{color:String::from("b"), number:10},
-                                       mp::Card{color:String::from("c"), number:3}];
+        state.state.player_hand = vec![mp::Card {
+                                           color: mp::Color::Color1,
+                                           number: 7,
+                                       },
+                                       mp::Card {
+                                           color: mp::Color::Color2,
+                                           number: 10,
+                                       },
+                                       mp::Card {
+                                           color: mp::Color::Color3,
+                                           number: 3,
+                                       }];
         let response = ai.update_game_state(&(state.state));
         assert_eq!("play 9 b,10", response);
-        state.state.player_hand = vec![mp::Card{color:String::from("a"), number:7},
-                                       mp::Card{color:String::from("c"), number:3}];
+        state.state.player_hand = vec![mp::Card {
+                                           color: mp::Color::Color1,
+                                           number: 7,
+                                       },
+                                       mp::Card {
+                                           color: mp::Color::Color3,
+                                           number: 3,
+                                       }];
         let response = ai.update_game_state(&(state.state));
         assert_eq!("play 9 a,7", response);
-        state.state.player_hand = vec![mp::Card{color:String::from("c"), number:3}];
+        state.state.player_hand = vec![mp::Card {
+                                           color: mp::Color::Color3,
+                                           number: 3,
+                                       }];
         let response = ai.update_game_state(&(state.state));
         assert_eq!("play 9 c,3", response);
     }
